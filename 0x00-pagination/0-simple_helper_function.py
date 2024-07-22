70a955742823296da5f56e75531019c27aab73b3
jedidia-nku <jedinkuze123@gmail.com>
2024-07-22 21:25:57 +0200

    0x00-pagination task

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,26 +1,12 @@
 #!/usr/bin/env python3
-
+"""Pagination helper function.
 """
-Module to provide pagination range.
-"""
-
 from typing import Tuple
 
+
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Retrieves the index range from a given page and page size.
     """
-    Function to return a tuple containing start and end index for pagination.
-
-    Args:
-    page (int): The current page number (1-indexed).
-    page_size (int): The number of items per page.
-
-    Returns:
-    Tuple[int, int]: A tuple containing the start index and end index for the pagination.
-    """
-
-    start_index = (page - 1) * page_size
-
-    end_index = page * page_size
-    
-    return start_index, end_index
-   
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
